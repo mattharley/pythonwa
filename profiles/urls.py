18f0b445fcafecf00e7cd783e43b6e81e06cae27
Deathridge <danielrbetteridge@gmail.com>
2016-05-11 15:55:09 +0800

    Added profiles model, view, list page template

---
+++ profiles/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from .models import Profile
+from .views import ProfileListView
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    url(r'^profiles/$', ProfileListView.as_view(queryset=Profile.objects.all())),   
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
