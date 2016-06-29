a9a414eaad7d3af0f96ed7ff29541f596e18ba70
Matt Harley <matt@mattharley.com>
2016-06-29 18:11:04 +0800

    updated requirements, updated static root due to heroku error

--- pdpdmeetup/settings.py
+++ pdpdmeetup/settings.py
@@ -121,7 +121,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
