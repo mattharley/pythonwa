ae3a0aba62fd1851c2f6d00e6c264277f009ac7e
Matt Harley <matt@mattharley.com>
2016-07-07 08:55:29 +0800

    test and fixes for divide by 0

--- demos/tests.py
+++ demos/tests.py
@@ -10,8 +10,10 @@ class DemoTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
         self.assertEqual(float(response.context['result']), 2.0)
+        self.assertEqual(len(response.context['errors']), 0)
 
     def test_divide_by_zero(self):
         response = self.client.get('/demos/divide?n=10&d=0')
 
         self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(response.context['errors']), 0)
--- demos/views.py
+++ demos/views.py
@@ -12,15 +12,31 @@ class DivideByView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(DivideByView, self).get_context_data(**kwargs)
-        context['numerator'] = float(self.request.GET.get('n', None))
-        context['denominator'] = float(self.request.GET.get('d', None))
-        logger.info(
-            'about to do this: {0} / {1}'.format(
-                context['numerator'],
-                context['denominator']
-            )
-        )
-        context['result'] = context['numerator'] / context['denominator']
+        context['errors'] = []
+        numerator = self.request.GET.get('n', None)
+        denominator = self.request.GET.get('d', None)
+
+        if numerator is None or denominator is None:
+            logger.error('you must set a numerator and denominator!')
+            context['errors'].append('you must set a numerator and denominator!')
+        else:
+            numerator = float(numerator)
+            denominator = float(denominator)
+
+            if denominator == 0.0:
+                logger.error('attempted to divide by zero!')
+                context['errors'].append('you tried to divide by zero!')
+            else:
+                logger.info(
+                    'about to do this: {0} / {1}'.format(
+                        numerator,
+                        denominator
+                    )
+                )
+                context['result'] = numerator / denominator
+
+        context['numerator'] = numerator
+        context['denominator'] = denominator
         return context
 
 class SleepView(TemplateView):
