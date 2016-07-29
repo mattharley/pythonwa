222dd93587443e136bdbbeb826ed609cb96d803e
Daniel Betteridge <danielrbetteridge@gmail.com>
2016-07-30 00:06:03 +0800

    Added link buttons for home, profiles , talks, companies to base template. removed wsgiref as incompatible with python3 and doesnt seem to be used. moved profiles page to use standard base template

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,5 @@ from .views import ProfileListView
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    url(r'^profiles/$', ProfileListView.as_view(queryset=Profile.objects.all())),   
+    url(r'^profiles/$', ProfileListView.as_view(queryset=Profile.objects.all()), name='profiles-page'),   
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
