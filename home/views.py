bb96bd73c358f2ae3bbb021db98be384acd074c0
James Prnich <james.prnich@gmail.com>
2016-08-20 13:20:36 +0800

    Simplified/cleaned up code. Improved naming and removed unnecessary datetime conversions.

--- home/views.py
+++ home/views.py
@@ -39,26 +39,18 @@ def ajax_meetups_tab(request, event_status):
     group_info = client.GetGroup({'urlname': 'Perth-Django-Users-Group'})
     group_events = client.GetEvents({'group_id': group_info.id, 'status': event_status})
 
-    array_event = []
+    events = []
 
     for event in reversed(group_events.results):
-        perth_tz = pytz.timezone('Australia/Perth')
 
-        event_datetime = event['time'] / 1000.0
-
-        my_date_in_perth = datetime.datetime.fromtimestamp(event_datetime, perth_tz).strftime('%Y-%m-%d %H:%M:%S')
-
-        # Convert datetime string to datetime object. This allows us to leverage 'humanize' within the template and display the dates in a more friendly way.
-        my_date_in_perth = datetime.datetime.strptime(my_date_in_perth, '%Y-%m-%d %H:%M:%S')
-
-        array_event.append({
+        events.append({
             'group_id': group_info.id,
             'event_id': event['id'],
             'event_name': event['name'],
             'event_address': event['venue']['address_1'],
             'event_description': event['description'],
             'event_yes_rsvp_count': event['yes_rsvp_count'],
-            'event_datetime': my_date_in_perth
+            'event_datetime': datetime.datetime.fromtimestamp(event['time'] / 1000.0, pytz.timezone('Australia/Perth'))
         })
 
     return render_to_response(
@@ -67,7 +59,7 @@ def ajax_meetups_tab(request, event_status):
             "group_info": group_info.__dict__.keys(),
             "group_name": group_info.name,
             "group_id": group_info.id,
-            "group_events": array_event,
+            "group_events": events,
 
             'event_status': event_status,
         })
