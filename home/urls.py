e5660bdd400ee5da714800d75859e29956d3cafb
Matt Harley <matt@mattharley.com>
2017-07-06 16:28:09 +0800

    added link to feedback form

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from home.views import home_page, ajax_meetups_tab
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 import home.views
 
@@ -9,6 +10,7 @@ urlpatterns = [
     url(r'^$', home.views.home_page, name='home-page'),
     url(r'^getinvolved/?$', home.views.get_involved, name='get-involved'),
     url(r'/meetupsapi/(?P<event_status>\w+)/?$', ajax_meetups_tab, name='ajax_meetups_tab'),
+    url(r'^feedback/?$',  RedirectView.as_view(url='https://docs.google.com/forms/d/e/1FAIpQLSem2CafmsLKWUFUQ2lFZGVbZTcOJtj5CrAcpKtUHvsjzNcMXw/viewform?usp=sf_linkm'), name='feedback'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
