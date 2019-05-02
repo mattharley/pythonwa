81d43b8fd5b59d0be94e3b3fde8dbc9027ec2dc2
Matt Harley <matt@mattharley.com>
2019-05-02 16:24:30 +0800

    a pretty container

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloPDPD/>
+    <el-row>
+      <el-col :span="16" :offset="4">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloPDPD></HelloPDPD>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -21,7 +25,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
