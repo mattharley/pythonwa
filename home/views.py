75196c2c2db0802ce45fd18ac9bfa3031a4f0ba0
Ben Fitzhardinge <ben.fitzhardinge@ausvet.com.au>
2019-08-26 17:11:14 +0800

    new meetup group events api call

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+import requests
 from django.shortcuts import render
 from django.utils.html import strip_tags
 from django.utils import dateformat
@@ -10,7 +13,8 @@ from meetup.exceptions import HttpClientError
 import datetime
 import pytz
 
-perth_timezone = pytz.timezone('Australia/Perth')
+PERTH_TIMEZONE = pytz.timezone('Australia/Perth')
+MEETUP_EVENTS_URL = 'https://api.meetup.com/Perth-Django-Users-Group/events/'
 
 
 def get_events(event_status, from_date):
@@ -35,7 +39,7 @@ def get_events(event_status, from_date):
                 'og_event_description': strip_tags(event['description']).encode('ascii', 'ignore'),
                 'event_yes_rsvp_count': event['yes_rsvp_count'],
                 'event_datetime': event_datetime,
-            })(datetime.datetime.fromtimestamp(event['time'] / 1000.0, perth_timezone))
+            })(datetime.datetime.fromtimestamp(event['time'] / 1000.0, PERTH_TIMEZONE))
             for event in sorted(group_events, key=lambda d: d['time']))
         return [
             event
@@ -44,6 +48,44 @@ def get_events(event_status, from_date):
         ]
     except HttpClientError:
         return []
+
+
+@lru_cache()
+def get_meetups():
+    """ get all the upcomming meetup events """
+    results = None
+    try:
+        response = requests.get(MEETUP_EVENTS_URL)
+        if response.status_code == 200:
+            results = [{
+                'event_id': event['id'],
+                'event_name': event['name'],
+                'event_url': event['link'],
+                'og_event_name': '({}) {}'.format(
+                    dateformat.format(
+                        datetime.datetime.fromtimestamp(
+                            event['time'] / 1000.0, PERTH_TIMEZONE
+                        ), 'D d M'
+                    ),
+                    event['name']
+                ),
+                'event_address': '{}, {}'.format(
+                    event['venue']['name'],
+                    event['venue']['address_1']
+                ) if 'venue' in event else '',
+                'event_description': event['description'],
+                'og_event_description': strip_tags(event['description']).encode('ascii', 'ignore'),
+                'event_yes_rsvp_count': event['yes_rsvp_count'],
+                'event_datetime': dateformat.format(
+                        datetime.datetime.fromtimestamp(
+                            event['time'] / 1000.0, PERTH_TIMEZONE
+                        ), 'D d M'
+                    ),
+            } for event in sorted(response.json(), key=lambda d: d['time'])]
+
+    except requests.exceptions.RequestException as e:
+        print(e)  # need to log this somehow
+    return results
 
 
 def home_page(request):
@@ -55,11 +97,12 @@ def home_page(request):
         args = tuple(
             user_num or default
             for default, user_num in zip_longest(default_args, user_args))
-        date = datetime.datetime(*args, tzinfo=perth_timezone)
+        date = datetime.datetime(*args, tzinfo=PERTH_TIMEZONE)
     else:
         date = timezone.now()
     try:
-        coming_event = get_events('upcoming', date)[0]
+        # coming_event = get_events('upcoming', date)[0]
+        coming_event = get_meetups()[0]
     except IndexError:
         coming_event = {
             'event_name': 'No upcoming event',
@@ -95,7 +138,8 @@ def ajax_meetups_tab(request, event_status):
     :param event_status: upcoming, past, proposed, suggested, cancelled, draft
     :return:
     """
-    events = get_events(event_status, timezone.now())
+    # events = get_events(event_status, timezone.now())
+    events = get_meetups()
 
     return render(
         request,
