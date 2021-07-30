8ccb47f409a5edcce7dae703c132f968a75e177f
Matt Harley <matt@mattharley.com>
2021-07-30 15:49:02 +0800

    formatting

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
     <footer class="mt-24 bg-gray-100 sm:mt-12">
-      <div class="mx-auto max-w-md py-6 px-4 overflow-hidden sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-        <p class="mt-4 text-center text-base text-gray-400">
-          &copy; 2021 Python WA
-        </p>
-      </div>
+        <div class="mx-auto max-w-md py-6 px-4 overflow-hidden sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+            <p class="mt-4 text-center text-base text-gray-400">
+                &copy; 2021 Python WA
+            </p>
+        </div>
     </footer>
 </template>
--- frontend/src/components/Photos.vue
+++ frontend/src/components/Photos.vue
@@ -1,34 +1,61 @@
 <template>
-  <section id="photos" class="md:max-w-xl mx-auto">
-    <div class="text-center">
-      <Carousel>
-        <Slide v-for="index in 8" :key="index">
-          <img :src="`/src/assets/photo${index}.jpeg`" />
-        </Slide>
-        <template #addons>
-          <Navigation />
-          <Pagination />
-        </template>
-      </Carousel>
-    </div>
-  </section>
+    <section id="photos" class="md:max-w-xl mx-auto">
+        <div class="text-center">
+            <Carousel>
+                <Slide v-for="index in 8" :key="index">
+                    <img :src="imageUrl(index)" />
+                </Slide>
+                <template #addons>
+                    <Navigation />
+                    <Pagination />
+                </template>
+            </Carousel>
+        </div>
+    </section>
 </template>
 
 <script>
-import 'vue3-carousel/dist/carousel.css';
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 
 export default {
-  components: {
-    Carousel,
-    Slide,
-    Pagination,
-    Navigation,
-  },
-  data() {
-    return {
-      items: new Array(8)
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation
+    },
+    data() {
+        return {
+            items: new Array(8),
+        };
+    },
+    methods: {
+        imageUrl(index) {
+            // need to do this because of the dynamic naming of images
+            // see: https://vitejs.dev/guide/assets.html#new-url-url-import-meta-url
+            return new URL(`../assets/photo${index}.jpeg`, import.meta.url);
+        }
     }
-  }
 };
 </script>
+
+<style>
+@tailwind screens;
+
+.carousel__prev {
+    transform: translate(25%, -50%);
+}
+.carousel__next {
+    transform: translate(-25%, -50%);
+}
+
+@screen md {
+    .carousel__prev {
+        transform: translate(-50%, -50%);
+    }
+    .carousel__next {
+        transform: translate(50%, -50%);
+    }
+}
+</style>
